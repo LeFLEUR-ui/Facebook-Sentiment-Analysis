e23f2f85a1c2f574678c317581f77cc2efa0d22c
Marvin Fabricante <marvinfabricante630@gmail.com>
2026-04-05 17:35:11 +0800

    making dashboard page private

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom'; // for redirection
+import { useNavigate } from 'react-router-dom';
 import {
   Chart as ChartJS,
   CategoryScale,
