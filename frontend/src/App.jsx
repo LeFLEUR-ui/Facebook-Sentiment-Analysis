784360aacb3b0833b81a0b6c544711c6c692b3f4
Marvin Fabricante <marvinfabricante630@gmail.com>
2026-04-05 17:36:36 +0800

    registering src/pages/Register.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={<Login />} /> 
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} /> 
         <Route path="/" element={<Dashboard /> } />
       </Routes>
     </Router>
